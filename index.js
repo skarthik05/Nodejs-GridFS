02655bcdfa509d3a24d776f5c23e46cfb03737f9
skarthik05 <karthiks0598@gmail.com>
2022-07-08 16:15:46 +0530

    added routes, middlwares and db files

--- index.js
+++ index.js
@@ -29,6 +29,50 @@ app.use("/file", upload);
 app.get("/", (req, res) => {
   return res.render("index", { title: "Express" });
 });
+/*
+@dec Listing all the files
+*/
+app.get("/files", async (req, res) => {
+  let files = await gfs.files.find({}).toArray();
+  if (!files?.length) return res.status(404).json({ message: "No files" });
+  files = files.reduce((acc, current) => {
+    current.isImage = current.contentType === "image/png" ? true : false;
+    const x = acc.find((file) => file.filename === current.filename);
+    if (!x) {
+      return acc.concat([current]);
+    } else {
+      return acc;
+    }
+  }, []);
+  return res.render("listingFiles", { files });
+});
+// media routes
+app.get("/image/:filename", async (req, res) => {
+  try {
+    const lates = await gfs.files.findOne({ filename: req.params.filename });
+    let latestVersion = await gfs.files
+      .find({ filename: req.params.filename })
+      .project({ _id: 1, contentType: 1 })
+      .sort({ _id: -1 })
+      .limit(1)
+      .toArray();
+    latestVersion = latestVersion[0];
+    const readStream = gfs.createReadStream(latestVersion._id);
+    readStream.pipe(res);
+  } catch (error) {
+    res.send("not found");
+  }
+});
+
+app.delete("/file/:filename", async (req, res) => {
+  try {
+    await gfs.files.deleteOne({ filename: req.params.filename });
+    res.send("success");
+  } catch (error) {
+    console.log(error);
+    res.send("An error occured.");
+  }
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
